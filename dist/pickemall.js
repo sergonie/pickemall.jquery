023d488d4ff4827d16c500e37bf7034a6c35c784
starasenko <STarasenko@relevantmedia.ru>
2016-06-23 17:57:11 +0300

    change default cursor and any fixes

--- dist/pickemall.js
+++ dist/pickemall.js
@@ -2876,7 +2876,7 @@ _html2canvas.Renderer.Canvas = function(options) {
             className: 'pickemall',
             buttonClass: 'pickemall-btn',
             activeClass: 'pickemall__on',
-            cursorPicker: "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABoElEQVR42q3WsUuCQRzGcbOsjERKJWhpUWhpdYzWmorq3yiDaqw/oKK2KBpbIgK3thaVIJAwwiVCTBKiIqGhKNO+B48goqDne/BBX/V93vfufnevLpdzrQd96NV7R5sHCzhDHEsYdCrcixiKqMoPFm16Yro/ghD6MYw1FOrCa+L6fUdtAnu4wDzWkW8SXtVwdXyBKC7xi2d8tAgv6gY6HqIhbOClLqwsFR0XNGwemwkNalgyunvTk2+84hM5hXttws3EriCFE+wiq4s8IY1V+GzDzclJHCCsCtqpmwdzgTlVl3X4PiL6PKyeZDQnZVXXWKdjbsITDeERHac06ZuqLlPCgXbDAwpPtQhP6PugqiuqddJW7Y/o5BscYrJFeMhmQv3aW25xjXMsY0rhyW7CfarjO4WbMizhHlcaLuvw2q6YVZB5/VIJmlX6oO+DNuFuTCsk3RBuPCo8YLunD2BLq7Kk5V8Lzyl8tNuHxmmTXTGvcH+3z9JZvNcFV7QrxrQldNXMwjjCH97Uk23M2O6Kzdo4jtUTrzYst9P/DBwPNO0fh+uBi2lURocAAAAASUVORK5CYII=')",
+            cursorPicker: 'crosshair',
             rgbResult: false,
             onChange: function (color) {
                 console.log('Your picked color:', color);
@@ -2907,7 +2907,7 @@ _html2canvas.Renderer.Canvas = function(options) {
                 $el.toggleClass(settings.activeClass);
                 if ($el.hasClass(settings.activeClass)) {
                     console.log('pickemall on');
-                    $all.css('cursor', 'pointer');
+                    $all.css('cursor', settings.cursorPicker);
                 } else {
                     console.log('pickemall off');
                     $all.css('cursor', 'default');
@@ -2919,7 +2919,7 @@ _html2canvas.Renderer.Canvas = function(options) {
                 //if toggler active
                 if ($el.hasClass(settings.activeClass)) {
                     var $target = $(e.target);
-                    console.log($target);
+                    
                     //if not toggler
                     if (!$target.is($el)) {
                         html2canvas(document.body, {
